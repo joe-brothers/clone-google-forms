9fc707cddd1d3ab76c868596fed0ad1d1d31ee52
Ahra Cho <choar816@postech.ac.kr>
2022-08-12 22:13:47 +0900

    Edit: style
    - 질문 수정시 질문제목 width 수정
    - focus 되어야만 질문의 type 보여주도록 수정

--- src/pages/AdminPage/components/FormContent/FormContent.js
+++ src/pages/AdminPage/components/FormContent/FormContent.js
@@ -7,16 +7,7 @@ import {
   changeTitleAt,
   addOptionAt,
 } from "../../../../redux/slices/contentSlice";
-import {
-  Space,
-  Typography,
-  Input,
-  Button,
-  Card,
-  Select,
-  Radio,
-  Checkbox,
-} from "antd";
+import { Space, Typography, Input, Button, Card, Select, Radio, Checkbox } from "antd";
 import { purple, grey } from "@ant-design/colors";
 import { OptionTextLong, OptionTextShort } from "./OptionText";
 import { EtcChoice } from "./EtcChoice";
@@ -49,163 +40,126 @@ export const FormContent = () => {
 
   return (
     <Space direction="vertical" size="large">
-      {questions.map(
-        (
-          { title, type, optionList, hasEtc, isRequired, isFocused },
-          indexQuestion
-        ) => {
-          return (
-            <Card
-              key={`question_${indexQuestion}`}
-              style={{
-                position: "relative",
-                boxShadow: `rgb(0 0 0 / ${isFocused ? 15 : 5}%) 0px 0px 10px 5px`,
-                border: `2px solid ${isFocused ? purple.primary : "#e4e4e4"}`,
-                borderRadius: 10,
-                width: 700,
-              }}
-              onClick={(e) => {
-                dispatch(unfocusTitleCard());
-                unfocusAllQuestions();
-                dispatch(focusQuestionAt({ index: indexQuestion }));
-              }}
-            >
-              {isFocused && (
-                <ButtonAddQuestion
-                  indexQuestion={indexQuestion}
-                  isTitle={false}
-                />
-              )}
-              <Space direction="vertical" style={{ width: "100%" }}>
-                <Space
-                  style={{ width: "100%", justifyContent: "space-between" }}
-                >
-                  {isFocused ? (
+      {questions.map(({ title, type, optionList, hasEtc, isRequired, isFocused }, indexQuestion) => {
+        return (
+          <Card
+            key={`question_${indexQuestion}`}
+            style={{
+              position: "relative",
+              boxShadow: `rgb(0 0 0 / ${isFocused ? 15 : 5}%) 0px 0px 10px 5px`,
+              border: `2px solid ${isFocused ? purple.primary : "#e4e4e4"}`,
+              borderRadius: 10,
+              width: 700,
+            }}
+            onClick={(e) => {
+              dispatch(unfocusTitleCard());
+              unfocusAllQuestions();
+              dispatch(focusQuestionAt({ index: indexQuestion }));
+            }}
+          >
+            {isFocused && <ButtonAddQuestion indexQuestion={indexQuestion} isTitle={false} />}
+            <Space direction="vertical" style={{ width: "100%" }}>
+              <Space style={{ width: "100%", justifyContent: "space-between" }}>
+                {isFocused ? (
+                  <>
                     <Input
                       size="large"
                       placeholder="질문"
                       value={title}
                       onChange={(e) => onChangeTitle(e, indexQuestion)}
+                      style={{ width: 400 }}
                     />
-                  ) : (
-                    <>
-                      <Space style={{ fontSize: 16 }}>
-                        {title || "질문"}
-                        {isRequired && <Text type="danger">*</Text>}
-                      </Space>
-                    </>
-                  )}
-                  <Select
-                    value={type}
-                    onSelect={(type) => {
-                      dispatch(
-                        changeQuestionType({ type, index: indexQuestion })
-                      );
-                    }}
-                    style={{ width: 120 }}
-                  >
-                    <Option value="textShort">단답형</Option>
-                    <Option value="textLong">장문형</Option>
-                    <Option value="radio">객관식 질문</Option>
-                    <Option value="checkbox">체크박스</Option>
-                    <Option value="dropdown">드롭다운</Option>
-                  </Select>
-                </Space>
-
-                {/* 답변 목록 (단답형/장문형/체크박스 등등) */}
-                {type === "textShort" && <OptionTextShort />}
-                {type === "textLong" && <OptionTextLong />}
-                {type === "radio" && (
-                  <Space direction="vertical">
-                    <OptionChoices
-                      ComponentToCheck={<Radio disabled />}
-                      optionList={optionList}
-                      indexQuestion={indexQuestion}
-                      isFocused={isFocused}
-                    />
-                    {hasEtc && (
-                      <EtcChoice
-                        ComponentToCheck={<Radio disabled />}
-                        isFocused={isFocused}
-                        index={indexQuestion}
-                      />
-                    )}
-                    {isFocused && (
-                      <AddOptionOrEtc
-                        ComponentToCheck={<Radio disabled />}
-                        hasEtc={hasEtc}
-                        index={indexQuestion}
-                      />
-                    )}
+                    <Select
+                      value={type}
+                      onSelect={(type) => {
+                        dispatch(changeQuestionType({ type, index: indexQuestion }));
+                      }}
+                      style={{ width: 120 }}
+                    >
+                      <Option value="textShort">단답형</Option>
+                      <Option value="textLong">장문형</Option>
+                      <Option value="radio">객관식 질문</Option>
+                      <Option value="checkbox">체크박스</Option>
+                      <Option value="dropdown">드롭다운</Option>
+                    </Select>
+                  </>
+                ) : (
+                  <Space style={{ fontSize: 16 }}>
+                    {title || "질문"}
+                    {isRequired && <Text type="danger">*</Text>}
                   </Space>
-                )}
-                {type === "checkbox" && (
-                  <Space direction="vertical">
-                    <OptionChoices
-                      ComponentToCheck={<Checkbox disabled />}
-                      optionList={optionList}
-                      indexQuestion={indexQuestion}
-                      isFocused={isFocused}
-                    />
-                    {hasEtc && (
-                      <EtcChoice
-                        ComponentToCheck={<Checkbox disabled />}
-                        isFocused={isFocused}
-                        index={indexQuestion}
-                      />
-                    )}
-                    {isFocused && (
-                      <AddOptionOrEtc
-                        ComponentToCheck={<Checkbox disabled />}
-                        hasEtc={hasEtc}
-                        index={indexQuestion}
-                      />
-                    )}
-                  </Space>
-                )}
-                {type === "dropdown" && (
-                  <Space direction="vertical">
-                    {optionList.map((option, indexOption) => (
-                      <Space style={{ fontSize: 14 }}>
-                        <Text>{indexOption + 1}</Text>
-                        {isFocused ? (
-                          <InputSentence
-                            optionList={optionList}
-                            option={option}
-                            indexOption={indexOption}
-                            indexQuestion={indexQuestion}
-                          />
-                        ) : (
-                          <Text>{option}</Text>
-                        )}
-                      </Space>
-                    ))}
-                    {isFocused && (
-                      <Space>
-                        <Text>{optionList.length + 1}</Text>
-                        <Button
-                          type="dashed"
-                          onClick={() => onClickAddOption(indexQuestion)}
-                        >
-                          옵션 추가
-                        </Button>
-                      </Space>
-                    )}
-                  </Space>
-                )}
-                {/* 답변 목록 끝 */}
-
-                {isFocused && (
-                  <QuestionSetting
-                    indexQuestion={indexQuestion}
-                    isRequired={isRequired}
-                  />
                 )}
               </Space>
-            </Card>
-          );
-        }
-      )}
+
+              {/* 답변 목록 (단답형/장문형/체크박스 등등) */}
+              {type === "textShort" && <OptionTextShort />}
+              {type === "textLong" && <OptionTextLong />}
+              {type === "radio" && (
+                <Space direction="vertical">
+                  <OptionChoices
+                    ComponentToCheck={<Radio disabled />}
+                    optionList={optionList}
+                    indexQuestion={indexQuestion}
+                    isFocused={isFocused}
+                  />
+                  {hasEtc && (
+                    <EtcChoice ComponentToCheck={<Radio disabled />} isFocused={isFocused} index={indexQuestion} />
+                  )}
+                  {isFocused && (
+                    <AddOptionOrEtc ComponentToCheck={<Radio disabled />} hasEtc={hasEtc} index={indexQuestion} />
+                  )}
+                </Space>
+              )}
+              {type === "checkbox" && (
+                <Space direction="vertical">
+                  <OptionChoices
+                    ComponentToCheck={<Checkbox disabled />}
+                    optionList={optionList}
+                    indexQuestion={indexQuestion}
+                    isFocused={isFocused}
+                  />
+                  {hasEtc && (
+                    <EtcChoice ComponentToCheck={<Checkbox disabled />} isFocused={isFocused} index={indexQuestion} />
+                  )}
+                  {isFocused && (
+                    <AddOptionOrEtc ComponentToCheck={<Checkbox disabled />} hasEtc={hasEtc} index={indexQuestion} />
+                  )}
+                </Space>
+              )}
+              {type === "dropdown" && (
+                <Space direction="vertical">
+                  {optionList.map((option, indexOption) => (
+                    <Space style={{ fontSize: 14 }}>
+                      <Text>{indexOption + 1}</Text>
+                      {isFocused ? (
+                        <InputSentence
+                          optionList={optionList}
+                          option={option}
+                          indexOption={indexOption}
+                          indexQuestion={indexQuestion}
+                        />
+                      ) : (
+                        <Text>{option}</Text>
+                      )}
+                    </Space>
+                  ))}
+                  {isFocused && (
+                    <Space>
+                      <Text>{optionList.length + 1}</Text>
+                      <Button type="dashed" onClick={() => onClickAddOption(indexQuestion)}>
+                        옵션 추가
+                      </Button>
+                    </Space>
+                  )}
+                </Space>
+              )}
+              {/* 답변 목록 끝 */}
+
+              {isFocused && <QuestionSetting indexQuestion={indexQuestion} isRequired={isRequired} />}
+            </Space>
+          </Card>
+        );
+      })}
     </Space>
   );
 };
