c910b752bce9deac4e5624c92b1db9bc4141a338
Ahra Cho <choar816@postech.ac.kr>
2022-08-12 17:24:04 +0900

    Add: 미리보기 페이지 UI

--- src/pages/PreviewPage/PreviewPage.js
+++ src/pages/PreviewPage/PreviewPage.js
@@ -1,19 +1,108 @@
 import { useSelector } from "react-redux";
-import { Space, Card, Typography, Select, Input } from "antd";
+import { Space, Card, Typography, Select, Input, Checkbox, Radio } from "antd";
 import "antd/dist/antd.min.css";
-const { Title } = Typography;
+const { Text, Title } = Typography;
+const { TextArea } = Input;
+const { Option } = Select;
 
 export const PreviewPage = () => {
   const { title, description } = useSelector((state) => state.formTitle);
   const { questions } = useSelector((state) => state.formContent);
 
+  const checkHasRequired = () => {
+    let hasRequired = false;
+    questions.forEach(({ isRequired }) => {
+      if (isRequired) hasRequired = true;
+    });
+    return hasRequired;
+  };
+
   return (
-    <Space direction="vertical" size="large" style={{ display: "flex" }}>
-      <h2>Preview Page</h2>
-      <Card style={{ width: 700 }}>
+    <Space
+      direction="vertical"
+      size="large"
+      style={{ display: "flex", width: 700, paddingTop: 50, paddingBottom: 50 }}
+    >
+      <Card>
         <Title level={2}>{title}</Title>
-        <p>{description}</p>
+        <Text>{description}</Text>
+        {checkHasRequired() && <Text type="danger">* 필수항목</Text>}
       </Card>
+      {questions.map(
+        (
+          { title, type, optionList, hasEtc, isRequired, isFocused },
+          indexQuestion
+        ) => {
+          return (
+            <Card key={`answer_${indexQuestion}`}>
+              <Space style={{ width: "100%", marginBottom: 8 }}>
+                <Text style={{ fontSize: 16 }}>{title}</Text>
+                {isRequired && <Text type="danger">*</Text>}
+              </Space>
+              {type === "textShort" && (
+                <Input style={{ width: "50%" }} placeholder="내 답변" />
+              )}
+              {type === "textLong" && (
+                <TextArea autoSize placeholder="내 답변" />
+              )}
+              {type === "radio" && (
+                <Radio.Group style={{ width: "100%" }}>
+                  <Space direction="vertical" style={{ width: "100%" }}>
+                    {optionList.map((option) => (
+                      <Radio value={option}>{option}</Radio>
+                    ))}
+                    {hasEtc && (
+                      <div
+                        style={{
+                          fontSize: 14,
+                          width: "100%",
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        <Radio style={{ flexShrink: 0 }}>기타:</Radio>
+                        <Input style={{ width: "100%", flexGrow: 1 }} />
+                      </div>
+                    )}
+                  </Space>
+                </Radio.Group>
+              )}
+              {type === "checkbox" && (
+                <>
+                  {optionList.map((option, optionIndex) => (
+                    <Space
+                      style={{ fontSize: 14, width: "100%", marginTop: 8 }}
+                    >
+                      <Checkbox>{option}</Checkbox>
+                    </Space>
+                  ))}
+                  {hasEtc && (
+                    <div
+                      style={{
+                        fontSize: 14,
+                        width: "100%",
+                        display: "flex",
+                        alignItems: "center",
+                        marginTop: 4,
+                      }}
+                    >
+                      <Checkbox style={{ flexShrink: 0 }}>기타:</Checkbox>
+                      <Input style={{ width: "100%", flexGrow: 1 }} />
+                    </div>
+                  )}
+                </>
+              )}
+              {type === "dropdown" && (
+                <Select placeholder="선택" style={{ width: 200 }}>
+                  {optionList.map((option) => (
+                    <Option value={option}>{option}</Option>
+                  ))}
+                </Select>
+              )}
+            </Card>
+          );
+        }
+      )}
     </Space>
   );
 };
